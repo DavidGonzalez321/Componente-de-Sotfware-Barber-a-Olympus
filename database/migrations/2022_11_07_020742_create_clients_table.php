aede28019797d9ea80af2db436ecee97f6c0e89a
DavidGonzalez321 <109323354+DavidGonzalez321@users.noreply.github.com>
2022-11-08 21:15:04 -0400

    Add files via upload

---
+++ database/migrations/2022_11_07_020742_create_clients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->integer('CI');
+            $table->timestamps();
+            $table->string('firtName');
+            $table->string('lastName');
+            $table->string('email');
+            $table->string('address');
+            $table->string('phoneNumber');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
